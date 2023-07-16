c9b057caca6533a33787c579f5c771d7e2e52ad2
hasmikmejlumyan <hasmik.mejlumyan@esterox.am>
2023-07-16 14:02:25 +0400

    [Lesson-15] added actions

--- src/main.js
+++ src/main.js
@@ -19,6 +19,17 @@ const store = createStore({
     },
   },
 
+  actions: {
+    increment(context) {
+      setTimeout(() => {
+        context.commit('increment');
+      }, 2000)
+    },
+    increase(context, payload) {
+      context.commit('increase', payload);
+    }
+  },
+
   getters: {
     finalCounter(state) {
       return state.counter * 3;
